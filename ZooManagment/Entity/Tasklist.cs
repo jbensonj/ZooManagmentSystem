10a77d19ff219220a6bbde4d48e9f34963a8d666
chickenfoot <63611976+jbensonj@users.noreply.github.com>
2024-04-26 02:38:35 -0400

    Update Tasklist.cs

--- ZooManagment/Entity/Tasklist.cs
+++ ZooManagment/Entity/Tasklist.cs
@@ -8,6 +8,8 @@ namespace ZooManagment.Entity
     {
         private List<Tasks> _tasklist = new List<Tasks>();
         public IReadOnlyList<Tasks> GetTasklist => _tasklist;
+        //<Summary>//
+        //Allows other classes to add to the tasklist by simply specify the tasklist and then passing a task into the add fuction//
         public void Add(Tasks task)
         {
             _tasklist.Add(task);
